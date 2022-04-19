99aa988af25a8f15f4657ab3370acb795eeb1d66
Konnattzu <hugo.flhs@outlook.fr>
2022-04-19 12:05:57 +0200

    liste

--- exemple/contenu.php
+++ exemple/contenu.php
@@ -4,11 +4,14 @@
 		include("inc.php/bddconnect.inc.php");
 		echo'<!DOCTYPE html>
 		<html lang="fr">
+		<head>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>';
 		if(isset($_GET) && isset($_GET["ref"]) && ($_GET["ref"] != "")) {
 			$page = $_GET["ref"];
 			$_SESSION["currentpage"] = $page;
 			include('header.php');
+			echo'</head>
+			<body>';
 			switch($page){
 				case "accueil":
 					include("inc.php/accueil.inc.php");
--- exemple/inc.php/liste.inc.php
+++ exemple/inc.php/liste.inc.php
@@ -4,7 +4,30 @@
 			<article>
 				<a href="index.php?ref=liste">Retour à l&apos; accueil</a>
 				<h1>La liste</h1>';
-				
+				$liste = mysqli_query($_SESSION["mysqli"], 'SELECT * FROM exemple;');
+				$i = 0;
+				echo'<table>';
+				while($data = $liste->fetch_assoc()){
+					$row[$i] = $data;
+					// if($i == 0){
+						// echo'<tr>';
+						// foreach ($row[$i] as $col => $val) {
+							// echo'<td>';
+							// echo $col;
+							// echo'</td>';
+						// }
+						// echo'</tr>';
+					// }
+					echo'<tr>';
+					foreach ($row[$i] as $col => $val) {
+						echo'<td>';
+						echo $val;
+						echo'</td>';
+					}
+					echo'</tr>';
+					$i++;
+				}
+				echo'</table>';
 			echo'</article>
 		</section>
 		';
