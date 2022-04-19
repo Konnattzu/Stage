02375e520c857c40881a1a5154ae7aa353bb5ef7
Konnattzu <hugo.flhs@outlook.fr>
2022-04-19 16:41:42 +0200

    table

--- exemple/inc.php/liste.inc.php
+++ exemple/inc.php/liste.inc.php
@@ -9,22 +9,60 @@
 				echo'<table>';
 				while($data = $liste->fetch_assoc()){
 					$row[$i] = $data;
-					// if($i == 0){
-						// echo'<tr>';
-						// foreach ($row[$i] as $col => $val) {
-							// echo'<td>';
-							// echo $col;
-							// echo'</td>';
-						// }
-						// echo'</tr>';
-					// }
+					if($i == 0){
+						echo'<tr>';
+						foreach ($row[$i] as $col => $val) {
+							echo'<td class="colname">';
+							$infos = mysqli_query($_SESSION["mysqli"], 'SELECT 
+												TABLE_CATALOG,
+												TABLE_SCHEMA,
+												TABLE_NAME, 
+												COLUMN_NAME, 
+												DATA_TYPE 
+												FROM INFORMATION_SCHEMA.COLUMNS
+												where TABLE_NAME = "exemple" 
+												and COLUMN_NAME = "'.$col.'";');
+							
+							echo $val." (";
+							while($desc = $infos->fetch_assoc()){
+								echo $desc["TABLE_CATALOG"].", ";
+								echo $desc["TABLE_SCHEMA"].", ";
+								echo $desc["TABLE_NAME"].", ";
+								echo $desc["COLUMN_NAME"].", ";
+								echo $desc["DATA_TYPE"].")";
+							}
+							echo'</td>';
+						}
+						echo'</tr>';
+					}else{
 					echo'<tr>';
 					foreach ($row[$i] as $col => $val) {
-						echo'<td>';
-						echo $val;
+							echo'<td>';
+						$infos = mysqli_query($_SESSION["mysqli"], 'SELECT 
+											TABLE_CATALOG,
+											TABLE_SCHEMA,
+											TABLE_NAME, 
+											COLUMN_NAME, 
+											DATA_TYPE 
+											FROM INFORMATION_SCHEMA.COLUMNS
+											where TABLE_NAME = "exemple" 
+											and COLUMN_NAME = "'.$col.'";');
+						
+						echo $val." (";
+						while($desc = $infos->fetch_assoc()){
+							echo $desc["TABLE_CATALOG"].", ";
+							echo $desc["TABLE_SCHEMA"].", ";
+							echo $desc["TABLE_NAME"].", ";
+							echo $desc["COLUMN_NAME"].", ";
+							echo $desc["DATA_TYPE"].")";
+						}
 						echo'</td>';
 					}
+						
 					echo'</tr>';
+					}
+					
+					
 					$i++;
 				}
 				echo'</table>';
