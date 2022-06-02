e6c8ce7238a60ec2cd03a8523b5b62eb7a9c5315
Konnattzu <hugo.flhs@outlook.fr>
2022-06-02 10:07:07 +0200

    Update bddconnect.php

--- exemple/bddconnect.php
+++ exemple/bddconnect.php
@@ -1,9 +1,9 @@
 <?php	
 	$BDD = array();
 	$BDD['host'] = "localhost";
-	$BDD['user'] = "12002444";
-	$BDD['pass'] = "070879959HF";
-	$BDD['db'] = "U978_insermdb";
+	$BDD['user'] = "root";
+	$BDD['pass'] = "bagnoletmmi76";
+	$BDD['db'] = "exemple";
 	$mysqli = mysqli_connect($BDD['host'], $BDD['user'], $BDD['pass'], $BDD['db']);
 	$mysqli->set_charset("utf8");
 	$_SESSION["mysqli"] = $mysqli;
