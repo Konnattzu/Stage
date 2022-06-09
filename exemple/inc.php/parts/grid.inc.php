54ca46b1b7d3d4727ec5064fbb3b45b5aa6d3a9d
Konnattzu <hugo.flhs@outlook.fr>
2022-06-09 14:03:27 +0200

    width

--- exemple/inc.php/parts/grid.inc.php
+++ exemple/inc.php/parts/grid.inc.php
@@ -258,7 +258,7 @@ const grid = new dhx.Grid(null, {
 css: "dhx_demo-grid",
 columns: [
   {
-      id: "action", gravity: 1.5, header: [{ text: "Actions", align: "center" }],
+      width: 75, id: "action", gravity: 1.5, header: [{ text: "Actions", align: "center" }],
       htmlEnable: true, align: "center",
       editable: false,
       autoWidth: false,
@@ -269,7 +269,7 @@ columns: [
 	  },';
       if(isset($header)){
 		for($i=0;$i<count($header)-1;$i++){
-				echo '{ id: "'.$header[$i].'", header: [{ text: "'.$header[$i].'", class: "numb"}, {content: "selectFilter"}], editable: true';
+				echo '{ width: 150, id: "'.$header[$i].'", header: [{ text: "'.$header[$i].'", class: "numb"}, {content: "selectFilter"}], editable: true';
                 if($datatype[$i] == "date"){
                     echo', type: "date", dateFormat: "%Y-%m-%d"';
                 }else if($datatype[$i] == "int"){
@@ -281,7 +281,7 @@ columns: [
                 }
                 echo' }, ';
 		}
-			echo '{ id: "'.$header[$i].'", header: [{ text: "'.$header[$i].'"}, {content: "selectFilter"}], editable: true } ';
+			echo '{ width: 150, id: "'.$header[$i].'", header: [{ text: "'.$header[$i].'"}, {content: "selectFilter"}], editable: true } ';
         }
     echo'
 ],
