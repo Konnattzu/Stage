a41077ee6d7f5d17300336c5289d950c9869e938
Konnattzu <hugo.flhs@outlook.fr>
2022-06-28 11:13:32 +0200

    oui

--- exemple/bddconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$pdo = new PDO('mysql:host=localhost;dbname=exemple','root','bagnoletmmi76');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-?>
