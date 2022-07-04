23347840fe310de0a7c26034f2a20685fdc5eaa6
Konnattzu <hugo.flhs@outlook.fr>
2022-07-04 09:47:30 +0200

    comment fix

--- exemple/inc.php/parts/get_com.inc.php
+++ exemple/inc.php/parts/get_com.inc.php
@@ -2,7 +2,7 @@
     include("../../bddconnect.php");
     $rowid = $_POST["row"];
     $colid = $_POST["column"];
-    $req = $pdo->prepare('SELECT * FROM commentaires WHERE patient_id="'.$rowid.'" AND colonne="'.$colid.'";');
+    $req = $pdo->prepare('SELECT * FROM commentaires WHERE identifiant="'.$rowid.'" AND colonne="'.$colid.'";');
     $req->execute();
     while($com = $req->fetch(PDO::FETCH_ASSOC)){
         echo $com["commentaire"];
--- exemple/inc.php/parts/send_com.inc.php
+++ exemple/inc.php/parts/send_com.inc.php
@@ -5,16 +5,16 @@
 	$row = $_POST['row'];
 	$header = $_POST['column'];
 	$comment = $_POST['comt'];
-	$query = $pdo->prepare('SELECT * FROM commentaires WHERE patient_id="'.$row.'" AND colonne="'.$header.'";');
+	$query = $pdo->prepare('SELECT * FROM commentaires WHERE identifiant="'.$row.'" AND colonne="'.$header.'";');
 	$query->execute();
 	$numrows = $query->fetch(PDO::FETCH_ASSOC);
 	if($numrows>=1){
-		echo 'UPDATE commentaires SET patient_id="'.$row.'", colonne="'.$header.'", commentaire="'.$comment.'";';
-		$query = 'UPDATE commentaires SET patient_id="'.$row.'", colonne="'.$header.'", commentaire="'.$comment.'";';
+		echo 'UPDATE commentaires SET commentaire="'.$comment.'" WHERE identifiant="'.$row.'" AND colonne="'.$header.'";';
+		$query = 'UPDATE commentaires SET commentaire="'.$comment.'" WHERE identifiant="'.$row.'" AND colonne="'.$header.'";';
         $pdo->exec($query);
 	}else{
-		echo "INSERT INTO commentaires (patient_id, colonne, commentaire) VALUES ('".$row."', '".$header."', '".$comment."');";
-		$query = 'INSERT INTO commentaires (patient_id, colonne, commentaire) VALUES ("'.$row.'", "'.$header.'", "'.$comment.'");';
+		echo "INSERT INTO commentaires (identifiant, colonne, commentaire) VALUES ('".$row."', '".$header."', '".$comment."');";
+		$query = 'INSERT INTO commentaires (identifiant, colonne, commentaire) VALUES ("'.$row.'", "'.$header.'", "'.$comment.'");';
         $pdo->exec($query);
 	}
 	
